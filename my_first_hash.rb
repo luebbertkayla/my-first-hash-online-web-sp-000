81317f5f6a6f9d0a026780f5e9ef0c2d93a123e5
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-05 01:26:35 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,43 +1,39 @@
+# Creating a hash
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  favorite_things = {
+    "favorite_animal" => "whale shark"
+  }
 end
 
-
+# Creating a hash with multiple key/value pairs
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {
+     "whale bone corsets" => 5, 
+      "porcelain vases" => 2, 
+      "oil paintings" => 3
+  }
 end
-
+ 
+# Looking up and returning the value of the "oil paintings" key by using []
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
+# Adding key/value pairs using []
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+                 
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
+  
+return shipping_manifest
 end
