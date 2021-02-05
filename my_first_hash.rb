9be72979af6399d9d46401b9d1018aa0c812f853
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-05 01:22:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,7 +31,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
   shipping_manifest["muskets"] = 2 
   shipping_manifest["gun powder"] = 4
+  
 return shipping_manifest
 end
